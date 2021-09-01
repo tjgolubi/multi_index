fa843a224d85b2c621d5866f5bb62a948e58c174
Terry Golubiewski <tjgolubi@netins.net>
2021-08-31 21:10:51 -0500

    Modernizes call_traits.hpp.

--- include/boost/multi_index/detail/call_traits.hpp
+++ include/boost/multi_index/detail/call_traits.hpp
@@ -26,88 +26,86 @@ namespace boost::multi_index::detail {
 
 template<typename T, bool small_>
 struct ct_imp2 {
-  typedef const T& param_type;
+  using param_type = const T&;
 };
 
 template<typename T>
 struct ct_imp2<T, true> {
-  typedef const T param_type;
+  using param_type = const T;
 };
 
 template<typename T, bool isp, bool b1, bool b2>
 struct ct_imp {
-  typedef const T& param_type;
+  using param_type = const T&;
 };
 
 template<typename T, bool isp, bool b2>
 struct ct_imp<T, isp, true, b2> {
-  typedef typename ct_imp2 < T, sizeof(T) <= sizeof(void*) >::param_type
-  param_type;
+  using param_type =
+                  typename ct_imp2<T, (sizeof(T) <= sizeof(void*))>::param_type;
 };
 
 template<typename T, bool isp, bool b1>
 struct ct_imp<T, isp, b1, true> {
-  typedef typename ct_imp2 < T, sizeof(T) <= sizeof(void*) >::param_type
-  param_type;
+  using param_type =
+                typename ct_imp2<T, (sizeof(T) <= sizeof(void*))>::param_type;
 };
 
 template<typename T, bool b1, bool b2>
 struct ct_imp<T, true, b1, b2> {
-  typedef const T param_type;
+  using param_type = const T;
 };
 
 template<typename T>
 struct call_traits {
 public:
-  typedef T value_type;
-  typedef T& reference;
-  typedef const T& const_reference;
+  using value_type      = T;
+  using reference       = T&;
+  using const_reference = const T&;
 
   // C++ Builder workaround: we should be able to define a compile time
-  // constant and pass that as a single template parameter to ct_imp<T,bool>,
+  // constant and pass that as a single template parameter to ct_imp<T, bool>,
   // however compiler bugs prevent this - instead pass three bool's to
-  // ct_imp<T,bool,bool,bool> and add an extra partial specialisation
+  // ct_imp<T, bool, bool, bool> and add an extra partial specialisation
   // of ct_imp to handle the logic. (JM)
-  typedef typename detail::ct_imp <
-  T,
-  std::is_pointer<T>::value,
-  std::is_arithmetic<T>::value,
-  std::is_enum<T>::value
-  >::param_type param_type;
-};
+  using param_type =
+      typename detail::ct_imp<T, std::is_pointer<T>::value,
+                                 std::is_arithmetic<T>::value,
+                                 std::is_enum<T>::value>::param_type;
+}; // call_traits
 
 template<typename T>
 struct call_traits<T&> {
-  typedef T& value_type;
-  typedef T& reference;
-  typedef const T& const_reference;
-  typedef T& param_type;  // hh removed const
+  using value_type      = T&;
+  using reference       = T&;
+  using const_reference = const T&;
+  using param_type      = T&;  // hh removed const
 };
 
 template<typename T, std::size_t N>
-struct call_traits<T [N]> {
+struct call_traits<T[N]> {
 private:
-  typedef T array_type[N];
+  using array_type      = T[N];
 public:
   // degrades array to pointer:
-  typedef const T* value_type;
-  typedef array_type& reference;
-  typedef const array_type& const_reference;
-  typedef const T* const param_type;
+  using value_type      = const T*;
+  using reference       = array_type&;
+  using const_reference = const array_type&;
+  using param_type      = const T* const;
 };
 
 template<typename T, std::size_t N>
-struct call_traits<const T [N]> {
+struct call_traits<const T[N]> {
 private:
-  typedef const T array_type[N];
+  using array_type      = const T[N];
 public:
   // degrades array to pointer:
-  typedef const T* value_type;
-  typedef array_type& reference;
-  typedef const array_type& const_reference;
-  typedef const T* const param_type;
+  using value_type      = const T*;
+  using reference       = array_type&;
+  using const_reference = const array_type&;
+  using param_type      = const T* const;
 };
 
-}
+} // boost::multi_index::detail
 
 #endif
